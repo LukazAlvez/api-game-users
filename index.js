453d86b369fe6cc317a97d8c32d4c0f30d6dc698
Lucas Alves <lc_1994@hotmai.com>
2025-04-09 23:19:04 -0300

    teste

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mongoose from 'mongoose';
+//import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import User from './models/user.js';
 
@@ -15,24 +15,27 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/users', async (req, res) => {
-    try{
-            const users = await User.find();
-            return res.json(users);
-        } catch (err) {
-            return res.status(500).json({ message: err.message });
-        }
-});
+// app.get('/users', async (req, res) => {
+//     try{
+//             const users = await User.find();
+//             return res.json(users);
+//         } catch (err) {
+//             return res.status(500).json({ message: err.message });
+//         }
+// });
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 //mongoose connection
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}).catch((err) => {
-    console.log(err);
-});
+// mongoose.connect(process.env.MONGO_URI)
+// .then(() => {
+//     console.log('Connected to MongoDB');
+//     app.listen(PORT, () => {
+//         console.log(`Server running on port ${PORT}`);
+//     });
+// }).catch((err) => {
+//     console.log(err);
+// });
 
