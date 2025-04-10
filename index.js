78977454bcd51c7f3b587886e92cf4656b12d7ce
Lucas Alves <lc_1994@hotmai.com>
2025-04-09 23:02:40 -0300

    config app

--- index.js
+++ index.js
@@ -1,48 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import dotenv from 'dotenv';
-import router from './routes/userRoutes.js';
+import User from './models/user.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//middleware
-app.use(cors());
 app.use(express.json());
 
-//routes
-app.use('/', router);
+app.get('/users', async (req, res) => {
+    try{
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+});
 
-
-let isConnected = false;
-async function connectToMongo() {
-  if (isConnected) return;
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    isConnected = true;
-    console.log('MongoDB conectado!');
-  } catch (err) {
-    console.error('Erro ao conectar ao MongoDB:', err);
-  }
-}
-
-// função serverless (exporta o handler)
-export default async function handler(req, res) {
-  await connectToMongo();
-  return app(req, res); // Vercel executa o Express como middleware
-}
 
 //mongoose connection
-//mongoose.connect(process.env.MONGO_URI)
-//.then(() => {
-//   console.log('Connected to MongoDB');
-//    app.listen(PORT, () => {
-//        console.log(`Server running on port ${PORT}`);
-//    });
-//}).catch((err) => {
-//    console.log(err);
-//});
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
+});
 
